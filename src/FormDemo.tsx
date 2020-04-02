2d97b3461fe2e44a75d5c5cd1ac7bebdd1cb2699
Duc Nguon <ducnguon@gmail.com>
2020-04-02 19:10:39 +0700

    Enhance fields with Material UI

--- src/FormDemo.tsx
+++ src/FormDemo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Form, Formik, Field } from "formik";
-import { Card, CardContent, Typography, TextField } from "@material-ui/core";
+import { Box, Button, Card, CardContent, Checkbox, CheckboxProps, FormControlLabel, FormGroup, MenuItem, TextField, Typography } from "@material-ui/core";
 import { InvestmentDetails } from "./InvestmentDetails";
 
 /**
@@ -30,30 +30,40 @@ export function FormDemo() {
                     {({values}) => (
                         <Form>
                             <Field name="fullName"  as={TextField} label="Full Name"/>
-                            <Field name="initialinvestment" type="number"/>
+                            <Field name="initialinvestment" type="number"  as={TextField}
+                    label="Initial Investment"/>
 
                             <Field name="investmentRisk" value="High" type="checkbox" />
                             <Field name="investmentRisk" value="Medium" type="checkbox" />
                             <Field name="investmentRisk" value="Low" type="checkbox" />
 
-                            <Field name="commentAboutInvesmentRisk" as="textarea"/>
-                            <Field name="commentAboutInvesmentRisk" as="select">
-                                <option value={0}>0</option>
-                                <option value={1}>1</option>
-                                <option value={2}>2</option>
-                                <option value={3}>3</option>
-                                <option value={4}>4</option>
-                                <option value={5}>5</option>
+                            <Field
+                                name="commentAboutInvestmentRisk"
+                                as={TextField}
+                                multiline
+                                rows={3}
+                                rowsMax={10}
+                                label="Comment About Investment Risk"
+                            />
+                            <Field name="commentAboutInvesmentRisk" as={TextField}
+                                select>
+                                <MenuItem value={-1}>Select ...</MenuItem>
+                                <MenuItem value={0}>0</MenuItem>
+                                <MenuItem value={1}>1</MenuItem>
+                                <MenuItem value={2}>2</MenuItem>
+                                <MenuItem value={3}>3</MenuItem>
+                                <MenuItem value={4}>4</MenuItem>
+                                <MenuItem value={5}>5</MenuItem>
                             </Field>
 
                             <Field name="acceptedTermsAndConditions" type="checkbox"></Field>
                             {/* Too see current values of the form */}
                             <pre>{JSON.stringify(values, null, 4)}</pre>
+                            {/* <pre>{JSON.stringify(errors, null, 4)}</pre> */}
+
                         </Form>
                     )}
                 </Formik>
-
-
             </CardContent>
         </Card>
     );
